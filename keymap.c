e78acc7f1f3424c01ad13464f0efeac2c0a7b245
J. W. Smith <jwsmith2spam at gmail dot com>
2022-01-03 17:01:55 -0500

    Major reorganization of layers
    "CURSR" is about right, that's what I was doing...
    
    (Hmm, I've already seen a small problem when typing 'ut' fast, I got
    F11...)
    
    I'm mostly happy with the placement of letter, number, and symbol keys.
    I'm not happy with layer and modifier keys :P  Even this layout is not
    ideal.
    
    I've discovered that my hands are way too small for an Ergodox.  The
    four outermost 1u keys in the thumb cluster are useless to me, as are
    the 1u keys next to 5 and 6.  Same story with the three keys below the
    Shift-Z-X row, and the equivalents on the right hand.  That's 16 keys
    that are hard or painful to use.  So it's clear I may need to
    investigate chording (or getting gud at using the bottommost keys).
    
    I could sell this self-made Ergodox, but probably not come close to
    recouping my investment.  To someone else, it's a custom product, a
    silly objet d'art.  One of its LEDs doesn't work, because I'm not good
    at soldering.  No one would find value in it, like they would a packaged
    Ergodox EZ.  To me, it was an attempt to make something that I could fix
    myself.  It was important to self-build because I wanted to be able to
    replace it should something break, so it's probably worth it for me to
    hang on to this keyboard and try to figure out how to use it.
    
    (I'd be interested in trying to switch keyboards if I got the ability to
    make my own PCBs and got my 3d printer working or a CNC.  None of this
    is in my near future.)
    
    The best route, as far as I'm concerned, is going to use this Ergodox
    like it's an Atreus or similar --- with minimal keys.  This is a start
    in that vein.
    
    ---
    
    The change here fixes an issue I was having, where I was routinely
    having problems holding down space, backspace, or enter (to get many
    newlines, spaces; or to delete many things) and getting nothing.  So
    I've moved my layer shifts onto home row keys.
    
    I split the layer shifts so that function keys and administrative
    stuff like Teensy reset live on one layer, and cursor navigation
    on another.
    
    I use the cursor stuff the most, that lives on 'F'.
    
    ---
    
    Future plans:
    
     - I might move the modifiers into tapdance bindings, if possible
     - I'd like to have 'launcher keys'
     - I've avoided putting things on WASD because games.  But even on PC
       I'm mostly a controller peon.  I only play FPSes with K&M, and only
       PC-first FPSes like Doom and not like Halo.  I don't play that many
       FPSes either.  But I might make a "gaming layer" that just sends key
       bindings and is otherwise a stock Ergodox layout.

--- keymap.c
+++ keymap.c
@@ -5,11 +5,14 @@
 enum layer_names {
     LAYER_BASE,
     LAYER_FUNC,
+    LAYER_CURSR,
     LAYER_MOUSE,
     LAYER_MEDIA
 };
 
 #define LED_LVL LED_LEVEL
+
+#define BSPC_WD LCTL(KC_BSPC)
 
 #define WEB_BCK LALT(KC_LEFT)
 #define WEB_FWD LALT(KC_RIGHT)
@@ -17,9 +20,11 @@ enum layer_names {
 #define WEB_NTB LALT(KC_T)
 #define WEB_CTB LALT(KC_W)
 
-#define SPC_FUN LT(LAYER_FUNC, KC_SPC)
-#define ENT_FUN LT(LAYER_FUNC, KC_ENTER)
-#define BCK_MOU LT(LAYER_MOUSE, KC_BSPC)
+#define R_FUNC LT(LAYER_FUNC, KC_R)
+#define U_FUNC LT(LAYER_FUNC, KC_U)
+#define F_CURSE LT(LAYER_CURSR, KC_F)
+#define V_MOUSE LT(LAYER_MOUSE, KC_V)
+
 #define MO_MEDA MO(LAYER_MEDIA)
 
 #define MOD_CTL OSM(MOD_LCTL)
@@ -29,36 +34,46 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT_ergodox_pretty(
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,    _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_ESC,
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_EQL,     KC_MINS, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-    KC_GRV,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC,    KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    _______, _______, MOD_ALT, MOD_WIN, MOD_CTL,                                        MOD_CTL, MOD_WIN, MOD_ALT, _______, _______,
-                                                 _______, _______,    _______, _______,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    R_FUNC,  KC_T,    KC_EQL,     KC_MINS, KC_Y,    U_FUNC,  KC_I,    KC_O,    KC_P,    KC_BSLS,
+    KC_GRV,  KC_A,    KC_S,    KC_D,    F_CURSE, KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    V_MOUSE, KC_B,    KC_LBRC,    KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    _______, _______, _______, MOD_WIN, MOD_CTL,                                        MOD_CTL, MOD_WIN, _______, _______, _______,
+                                                 MOD_ALT, _______,    _______, MOD_ALT,
                                                           _______,    _______,
-                                        SPC_FUN, BCK_MOU, _______,    _______, KC_DEL,  ENT_FUN
+                                        KC_SPC,  KC_BSPC, _______,    _______, KC_TAB,  KC_ENT
   ),
   [LAYER_FUNC] = LAYOUT_ergodox_pretty(
-    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,    _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_ESC,
-    _______, _______, _______, _______, _______, KC_F11,  KC_F11,     KC_F12,  KC_F12,  _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______,                      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______,    _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,
-    _______, _______, _______, _______, _______,                                        _______, _______, _______, _______, _______,
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,    _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
+    _______, _______, _______, _______, XXXXXXX, KC_F11,  KC_F11,     KC_F12,  KC_F12,  XXXXXXX, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______,                                        KC_PSCR, _______, _______, _______, _______,
                                                  _______, _______,    RESET,   LED_LVL,
                                                           _______,    _______,
-                                        _______, MO_MEDA, _______,    _______, KC_CAPS, KC_PSCREEN
+                                        _______, _______, _______,    KC_CAPS, _______, _______
+  ),
+  [LAYER_CURSR] = LAYOUT_ergodox_pretty(
+    _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______,                      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
+    _______, _______, _______, _______, MO_MEDA, _______, _______,    BSPC_WD, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,
+    _______, _______, _______, _______, _______,                                        _______, _______, _______, _______, _______,
+                                                 _______, _______,    _______, _______,
+                                                          _______,    _______,
+                                        _______, _______, _______,    _______, KC_DEL,  KC_BSPC
   ),
   [LAYER_MOUSE] = LAYOUT_ergodox_pretty(
-    KC_ESC,  _______, _______, _______, _______, _______, _______,    KC_ACL0, _______, _______, _______, _______, _______, KC_ESC,
+    _______, _______, _______, _______, _______, _______, _______,    KC_ACL0, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______,    KC_ACL1, WEB_CTB, WEB_BCK, WEB_FWD, WEB_NTB, _______, _______,
-    _______, _______, _______, _______, _______, _______,                      KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,
+    _______, _______, _______, _______, MO_MEDA, _______,                      KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,
     _______, _______, _______, _______, _______, _______, _______,    KC_ACL2, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______, _______,
     _______, _______, _______, _______, _______,                                        KC_BTN3, _______, _______, _______, _______,
                                                  _______, _______,    _______, _______,
                                                           _______,    _______,
-                                        MO_MEDA, _______, _______,    _______, KC_BTN2, KC_BTN1
+                                        _______, _______, _______,    _______, KC_BTN2, KC_BTN1
   ),
   [LAYER_MEDIA] = LAYOUT_ergodox_pretty(
-    KC_ESC,  _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, KC_ESC,
+    _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______,                      KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, _______, _______,
     _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
