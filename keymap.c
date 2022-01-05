6b4165115d00fd7dd8a62861d39d822307abecef
J. W. Smith <jwsmith2spam at gmail dot com>
2022-01-04 22:58:16 -0500

    Moved mouse LT from V to G
    G feels marginally more comfortable to press than V, to me.

--- keymap.c
+++ keymap.c
@@ -23,7 +23,7 @@ enum layer_names {
 #define R_FUNC LT(LAYER_FUNC, KC_R)
 #define U_FUNC LT(LAYER_FUNC, KC_U)
 #define F_CURSE LT(LAYER_CURSR, KC_F)
-#define V_MOUSE LT(LAYER_MOUSE, KC_V)
+#define G_MOUSE LT(LAYER_MOUSE, KC_G)
 
 #define MO_MEDA MO(LAYER_MEDIA)
 
@@ -35,8 +35,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT_ergodox_pretty(
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,    _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_ESC,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    R_FUNC,  KC_T,    KC_EQL,     KC_MINS, KC_Y,    U_FUNC,  KC_I,    KC_O,    KC_P,    KC_BSLS,
-    KC_GRV,  KC_A,    KC_S,    KC_D,    F_CURSE, KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    V_MOUSE, KC_B,    KC_LBRC,    KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    KC_GRV,  KC_A,    KC_S,    KC_D,    F_CURSE, G_MOUSE,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC,    KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
     _______, _______, _______, MOD_WIN, MOD_CTL,                                        MOD_CTL, MOD_WIN, _______, _______, _______,
                                                  MOD_ALT, _______,    _______, MOD_ALT,
                                                           _______,    _______,
@@ -55,8 +55,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_CURSR] = LAYOUT_ergodox_pretty(
     _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______,                      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
-    _______, _______, _______, _______, MO_MEDA, _______, _______,    BSPC_WD, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,
+    _______, _______, _______, _______, _______, MO_MEDA,                      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______,    BSPC_WD, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,
     _______, _______, _______, _______, _______,                                        _______, _______, _______, _______, _______,
                                                  _______, _______,    _______, _______,
                                                           _______,    _______,
