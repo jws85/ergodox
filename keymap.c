828f03ccace72327c9eab7ff2b38e2a70e5e09ea
J. W. Smith <jwsmith2spam at gmail dot com>
2022-01-07 20:04:56 -0500

    Add 'game' layer
    The idea here is to be like the base keymap, but as vanilla as possible
    in terms of layers.
    
    Most of the time, I can use my base layer to edit text and do other
    things on my computer.  But if I were ever playing a FPS or anything
    else using WASD movement, I can keep those binds without having to worry
    about triggering layers by holding down keys.  This frees me up to bind
    the left-hand homerow keys in my base layer.

--- keymap.c
+++ keymap.c
@@ -7,7 +7,8 @@ enum layer_names {
     LAYER_FUNC,
     LAYER_CURSR,
     LAYER_MOUSE,
-    LAYER_MEDIA
+    LAYER_MEDIA,
+    LAYER_GAMES,
 };
 
 #define LED_LVL LED_LEVEL
@@ -26,6 +27,8 @@ enum layer_names {
 #define D_MOUSE LT(LAYER_MOUSE, KC_D)
 
 #define MO_MEDA MO(LAYER_MEDIA)
+#define TO_GAME TO(LAYER_GAMES)
+#define TO_BASE TO(LAYER_BASE)
 
 #define MOD_CTL OSM(MOD_LCTL)
 #define MOD_ALT OSM(MOD_LALT)
@@ -52,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______,                                        KC_PSCR, _______, _______, _______, _______,
                                                  _______, _______,    RESET,   LED_LVL,
                                                           _______,    _______,
-                                        _______, _______, _______,    KC_CAPS, _______, _______
+                                        _______, _______, _______,    KC_CAPS, _______, TO_GAME
   ),
   [LAYER_CURSR] = LAYOUT_ergodox_pretty(
     _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
@@ -83,6 +86,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                  _______, _______,    _______, _______,
                                                           _______,    _______,
                                         _______, _______, _______,    _______, KC_MSTP, KC_MPLY
+  ),
+  [LAYER_GAMES] = LAYOUT_ergodox_pretty(
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    TO_BASE,    TO_BASE, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_ESC,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_EQL,     KC_MINS, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+    KC_GRV,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC,    KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    _______, _______, _______, MOD_WIN, MOD_CTL,                                        MOD_CTL, MOD_WIN, _______, _______, _______,
+                                                 MOD_ALT, TO_BASE,    TO_BASE, MOD_ALT,
+                                                          TO_BASE,    TO_BASE,
+                                        KC_SPC,  KC_BSPC, TO_BASE,    TO_BASE, KC_DEL,  KC_ENT
   ),
 };
 
